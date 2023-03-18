2615ffaa11f83c2856841e197f4f5bd0faa81675
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-03-18 13:24:20 +0200

    feat: add basic routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return <div>(404) Page NotFound!!! \_(^;^)_/</div>;
+  }
+}
+
+export default NotFound;
