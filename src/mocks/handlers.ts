694dea01165bd8e8d9120fd8190537a983a3c44a
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-04-09 20:49:32 +0300

    feat: add test for search request

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,17 @@
 import { rest } from 'msw';
 import { baseUrl } from '../configs/api';
 import photosData from '../data/photos';
+import searchTestPhotosData from '../data/searchTestPhotos';
 
 const endpoint = '/photos';
+const searchPotosEndpoint = '/search/photos';
 
 export const handlers = [
   rest.get(`${baseUrl}${endpoint}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(photosData));
+  }),
+
+  rest.get(`${baseUrl}${searchPotosEndpoint}`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(searchTestPhotosData));
   }),
 ];
--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,5 +1,6 @@
 import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Home from '../pages/Home';
 
@@ -7,5 +8,19 @@ describe('Home', () => {
   it('Fetch and display photo cards', async () => {
     const { findByText } = render(<Home />);
     expect(await findByText('a large city with lots of tall buildings')).toBeInTheDocument();
+  });
+
+  it('Fetch and display photo cards using search', async () => {
+    const { findByText, getByRole } = render(<Home />);
+
+    userEvent.type(getByRole('searchbox'), 'test');
+
+    await waitFor(() => {
+      expect(getByRole('searchbox')).toHaveValue('test');
+    });
+
+    userEvent.click(getByRole('button'));
+
+    expect(await findByText('Ben Mullins')).toBeInTheDocument();
   });
 });
